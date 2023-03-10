0949a368caad289560291c83c29ed7f24f8d89d4
Kumarion <121711454+Kumarion@users.noreply.github.com>
2023-03-09 20:55:44 -0500

    Fixed redirect

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,4 @@
 import { createTRPCRouter } from "~/server/api/trpc";
-import { exampleRouter } from "~/server/api/routers/example";
 import { slugRouter } from "~/server/api/routers/slug";
 
 /**
@@ -8,7 +7,6 @@ import { slugRouter } from "~/server/api/routers/slug";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
   slug: slugRouter,
 });
 
